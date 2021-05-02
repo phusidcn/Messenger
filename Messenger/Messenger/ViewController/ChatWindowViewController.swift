96d52dd47e8534d668236326bf657c680284cb04
sihlp <sihlp@vng.com.vn>
2021-05-03 01:47:25 +0700

    Summary: Change data model - More Detail:
    - Reason(s): N/A
    
    Issue fixed:
    
    - [x]
    
    Pulled By: sihlp

--- Messenger/Messenger/ViewController/ChatWindowViewController.swift
+++ Messenger/Messenger/ViewController/ChatWindowViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import ChatViewController
 
-class ChatWindowViewController: UIViewController {
+class ChatWindowViewController: ChatViewController {
     var cooridinator: ChatWindowCoordinator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func 
 
 }
--- Messenger/Pods/Target Support Files/Pods-Messenger-MessengerUITests/Pods-Messenger-MessengerUITests-frameworks.sh
+++ Messenger/Pods/Target Support Files/Pods-Messenger-MessengerUITests/Pods-Messenger-MessengerUITests-frameworks.sh
@@ -175,11 +175,15 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ChatViewController/ChatViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PlaceholderUITextView/PlaceholderUITextView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyButton/SwiftyButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyOnboard/SwiftyOnboard.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ChatViewController/ChatViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PlaceholderUITextView/PlaceholderUITextView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyButton/SwiftyButton.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyOnboard/SwiftyOnboard.framework"
