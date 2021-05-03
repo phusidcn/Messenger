061cdc3c756e06d696ae9beeb404846ade0a8a72
sihlp <sihlp@vng.com.vn>
2021-05-03 07:29:26 +0700

    Summary: Fix data model - More Detail:
    - Reason(s): N/A
    
    Issue fixed:
    
    - [x]
    
    Pulled By: sihlp

--- Messenger/Messenger/ViewController/ChatWindowViewController.swift
+++ Messenger/Messenger/ViewController/ChatWindowViewController.swift
@@ -22,7 +22,5 @@ class ChatWindowViewController: ChatViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
-    
-    func 
 
 }
