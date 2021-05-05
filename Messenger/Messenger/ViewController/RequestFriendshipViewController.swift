d7d3d95231cd7a7da278570521ee8b5d060f135b
sihlp <sihlp@vng.com.vn>
2021-05-06 00:48:46 +0700

    Add complete chat view controller

---
+++ Messenger/Messenger/ViewController/RequestFriendshipViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RequestFriendshipViewController.swift
+//  Messenger
+//
+//  Created by Huynh Lam Phu Si on 5/4/21.
+//
+
+import UIKit
+
+class RequestFriendshipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
